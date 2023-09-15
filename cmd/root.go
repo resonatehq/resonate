dad6befbf12439181bb94136836b1487941ffb26
David Farr <david@resonatehq.io>
2023-09-14 18:04:09 -0700

    Update dst workflow

--- cmd/root.go
+++ cmd/root.go
@@ -22,6 +22,7 @@ func init() {
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (defaults to resonate.yml)")
 	rootCmd.PersistentFlags().Int("log-level", 0, "log level")
 	_ = viper.BindPFlag("logs.level", rootCmd.PersistentFlags().Lookup("log-level"))
+	_ = viper.BindPFlag("dst.logs.level", rootCmd.PersistentFlags().Lookup("log-level"))
 }
 
 func initConfig() {
