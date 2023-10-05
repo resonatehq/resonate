6765d165da96246f174ce8366ad1083a8fef043d
David Farr <david@resonatehq.io>
2023-10-04 19:41:44 -0700

    test

--- test/dst/dst_test.go
+++ test/dst/dst_test.go
@@ -1,7 +1,6 @@
 package dst
 
 import (
-	"fmt"
 	"math/rand" // nosemgrep
 	"testing"
 	"time"
@@ -25,110 +24,93 @@ func TestDST(t *testing.T) {
 	reg := prometheus.NewRegistry()
 	metrics := metrics.New(reg)
 
-	for i := 0; i < 2; i++ {
-		// config
-		config := &system.Config{
-			NotificationCacheSize: 100,
-			SubmissionBatchSize:   100,
-			CompletionBatchSize:   100,
-		}
+	// config
+	config := &system.Config{
+		NotificationCacheSize: 100,
+		SubmissionBatchSize:   100,
+		CompletionBatchSize:   100,
+	}
 
-		// instatiate api/aio
-		api := api.New(1000, metrics)
-		aio := aio.NewDST(r)
+	// instatiate api/aio
+	api := api.New(1000, metrics)
+	aio := aio.NewDST(r)
 
-		// instatiate aio subsystems
-		network := network.NewDST(&network.ConfigDST{P: 0.5}, r)
-		store, err := sqlite.New(&sqlite.Config{Path: ":memory:", TxTimeout: 250 * time.Millisecond})
-		if err != nil {
-			t.Fatal(err)
-		}
+	// instatiate aio subsystems
+	network := network.NewDST(&network.ConfigDST{P: 0.5}, r)
+	store, err := sqlite.New(&sqlite.Config{Path: ":memory:", TxTimeout: 250 * time.Millisecond})
+	if err != nil {
+		t.Fatal(err)
+	}
 
-		// add api subsystems
-		aio.AddSubsystem(t_aio.Network, network)
-		aio.AddSubsystem(t_aio.Store, store)
+	// add api subsystems
+	aio.AddSubsystem(t_aio.Network, network)
+	aio.AddSubsystem(t_aio.Store, store)
 
-		// instantiate system
-		system := system.New(api, aio, config, metrics)
-		system.AddOnRequest(t_api.ReadPromise, coroutines.ReadPromise)
-		system.AddOnRequest(t_api.SearchPromises, coroutines.SearchPromises)
-		system.AddOnRequest(t_api.CreatePromise, coroutines.CreatePromise)
-		system.AddOnRequest(t_api.CancelPromise, coroutines.CancelPromise)
-		system.AddOnRequest(t_api.ResolvePromise, coroutines.ResolvePromise)
-		system.AddOnRequest(t_api.RejectPromise, coroutines.RejectPromise)
-		system.AddOnRequest(t_api.ReadSubscriptions, coroutines.ReadSubscriptions)
-		system.AddOnRequest(t_api.CreateSubscription, coroutines.CreateSubscription)
-		system.AddOnRequest(t_api.DeleteSubscription, coroutines.DeleteSubscription)
+	// instantiate system
+	system := system.New(api, aio, config, metrics)
+	system.AddOnRequest(t_api.ReadPromise, coroutines.ReadPromise)
+	system.AddOnRequest(t_api.SearchPromises, coroutines.SearchPromises)
+	system.AddOnRequest(t_api.CreatePromise, coroutines.CreatePromise)
+	system.AddOnRequest(t_api.CancelPromise, coroutines.CancelPromise)
+	system.AddOnRequest(t_api.ResolvePromise, coroutines.ResolvePromise)
+	system.AddOnRequest(t_api.RejectPromise, coroutines.RejectPromise)
+	system.AddOnRequest(t_api.ReadSubscriptions, coroutines.ReadSubscriptions)
+	system.AddOnRequest(t_api.CreateSubscription, coroutines.CreateSubscription)
+	system.AddOnRequest(t_api.DeleteSubscription, coroutines.DeleteSubscription)
+	system.AddOnTick(2, coroutines.TimeoutPromises)
+	system.AddOnTick(10, coroutines.NotifySubscriptions)
 
-		var reqs []t_api.Kind
-		if i == 0 {
-			// promises only
-			reqs = []t_api.Kind{
-				t_api.ReadPromise,
-				t_api.CreatePromise,
-				t_api.CancelPromise,
-				t_api.ResolvePromise,
-				t_api.RejectPromise,
-			}
+	// specify reqs to enable
+	reqs := []t_api.Kind{
+		t_api.ReadPromise,
+		t_api.SearchPromises,
+		t_api.CreatePromise,
+		t_api.CancelPromise,
+		t_api.ResolvePromise,
+		t_api.RejectPromise,
+		t_api.ReadSubscriptions,
+		t_api.CreateSubscription,
+		t_api.DeleteSubscription,
+	}
 
-		} else {
-			reqs = []t_api.Kind{
-				t_api.ReadPromise,
-				t_api.SearchPromises,
-				t_api.CreatePromise,
-				t_api.CancelPromise,
-				t_api.ResolvePromise,
-				t_api.RejectPromise,
-				t_api.ReadSubscriptions,
-				t_api.CreateSubscription,
-				t_api.DeleteSubscription,
-			}
+	// start api/aio
+	if err := api.Start(); err != nil {
+		t.Fatal(err)
+	}
+	if err := aio.Start(); err != nil {
+		t.Fatal(err)
+	}
 
-			system.AddOnTick(2, coroutines.TimeoutPromises)
-			system.AddOnTick(10, coroutines.NotifySubscriptions)
-		}
+	dst := New(&Config{
+		Ticks:           1000,
+		Reqs:            func() int { return 100 },
+		Ids:             100,
+		IdempotencyKeys: 100,
+		Headers:         100,
+		Data:            100,
+		Tags:            100,
+		Urls:            100,
+		Retries:         100,
+	})
 
-		// start api/aio
-		if err := api.Start(); err != nil {
-			t.Fatal(err)
-		}
-		if err := aio.Start(); err != nil {
-			t.Fatal(err)
-		}
+	if errs := dst.Run(r, api, aio, system, reqs); len(errs) > 0 {
+		t.Fatal(errs)
+	}
 
-		dst := New(&Config{
-			Ticks:           1000,
-			Reqs:            func() int { return 10 },
-			Ids:             100,
-			IdempotencyKeys: 100,
-			Headers:         100,
-			Data:            100,
-			Tags:            100,
-			Urls:            100,
-			Retries:         100,
-		})
+	// shutdown api/aio
+	api.Shutdown()
+	aio.Shutdown()
 
-		t.Run(fmt.Sprintf("i=%d, dst=%s", i, dst), func(t *testing.T) {
-			if errors := dst.Run(r, api, aio, system, reqs); len(errors) > 0 {
-				t.Fatal(errors[0])
-			}
-		})
+	// reset store
+	if err := store.Reset(); err != nil {
+		t.Fatal(err)
+	}
 
-		// shutdown api/aio
-		api.Shutdown()
-		aio.Shutdown()
-
-		// reset store
-		if err := store.Reset(); err != nil {
-			t.Fatal(err)
-		}
-
-		// stop api/aio
-		if err := api.Stop(); err != nil {
-			t.Fatal(err)
-		}
-		if err := aio.Stop(); err != nil {
-			t.Fatal(err)
-		}
+	// stop api/aio
+	if err := api.Stop(); err != nil {
+		t.Fatal(err)
+	}
+	if err := aio.Stop(); err != nil {
+		t.Fatal(err)
 	}
 }
