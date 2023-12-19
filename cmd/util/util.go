19d8ade1f034bd91267c2186c6f816f738a020b8
Gabriel Guerra <guergabo@gabriels-mbp.mynetworksettings.com>
2023-12-18 19:45:10 -0500

    feat(cleanup): x3

--- cmd/util/util.go
+++ cmd/util/util.go
@@ -1,27 +1,9 @@
 package util
 
-import (
-	"fmt"
-	"os"
-
-	"github.com/spf13/cobra"
-)
-
 func Unwrap[T comparable](value T) *T {
 	var zero T
 	if value == zero {
 		return nil
 	}
 	return &value
-}
-
-func Errorf(format string, v ...any) {
-	out := fmt.Sprintf(format, v...)
-	fmt.Println(out)
-	os.Exit(1)
-}
-
-func Write(cmd *cobra.Command, format string, v ...any) {
-	out := fmt.Sprintf(format, v...)
-	_, _ = cmd.OutOrStdout().Write([]byte(out))
 }
