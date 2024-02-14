8889ef7def4b0c9dd853b96ab13db972f5a66c3d
Vipul Vaibhaw <vaibhaw.vipul@gmail.com>
2024-02-14 22:38:58 +0530

    Store Unit Tests (#48)
    * adding lock unit tests
    
    * adding unit tests for schedules
    
    * adding promise unit tests
    
    * removing unused imports
    
    * Revert "removing unused imports"
    
    This reverts commit 5561fe4b82b289546515c9ccaa6a0c80b6c9b72f.
    
    * Revert "adding promise unit tests"
    
    This reverts commit eb94b1549db24b8c5278de9044c0bdf0166b0edf.
    
    * 1. renaming store to promisetransition test
    
    * remove duplicate test
    
    * remove the hacky tests
    
    * fix lint error
    
    * removing old tests from schedule
    
    * adding cleanup after test
    
    * fix lint errors
    
    * adding more scheudles for better testing
    
    * asserting the ids in wildcard search
    
    * explicitly checking schedule properties while creating a schedule that exists
    
    * enabling remote tests in cicd
    
    * clone the github resonate repo before build
    
    * adding the http git url for clone
    
    * fix path of the cloned repo
    
    * fix go version
    
    * starting resonate in bg
    
    * commenting out lint check to run tests
    
    * adding lint tests
    
    * lint changes
    
    * reordering steps
    
    * starting server before tests
    
    * an experiment
    
    * run lint with working-dir
    
    * code refactor
    
    * commenting lint
    
    * moving lint check before cloning the repo
    
    * remove numbers from promise test
    rename test suites
    rename test file to promiseTransition
    fix cicd:
    
    * rename the file and nit

--- impl/sdk/ts/test/lock.test.ts
+++ impl/sdk/ts/test/lock.test.ts
@@ -1,92 +1,80 @@
 import { jest, describe, test, expect } from "@jest/globals";
-import { Resonate, Context } from "../lib/resonate";
 import { LocalStore } from "../lib/core/stores/local";
-import { RemoteLockStore, RemoteStore } from "../lib/core/stores/remote";
+import { RemoteStore } from "../lib/core/stores/remote";
 import { Logger } from "../lib/core/loggers/logger";
 
 jest.setTimeout(50000);
 
-const sharedResource: string[] = [];
-
-function write(context: Context, id: string, final: boolean) {
-  return new Promise((resolve) => {
-    sharedResource.push(id);
-
-    if (final) {
-      resolve(sharedResource);
-    }
-  });
-}
-
-describe("Lock Store Tests", () => {
+describe("Store: Locks", () => {
   const useDurable = process.env.USE_DURABLE === "true";
   const url = process.env.RESONATE_URL || "http://localhost:8001";
 
   const store = useDurable ? new RemoteStore(url, "process-id", new Logger()) : new LocalStore();
-  const r1 = new Resonate({ store });
-  const r2 = new Resonate({ store });
 
-  r1.register("write", write, r1.options({ eid: "a", timeout: 5000 }));
-  r2.register("write", write, r2.options({ eid: "b", timeout: 5000 }));
+  test("Lock Store: Acquire lock that exists", async () => {
+    // register a lock
+    const resourceId = "existing-resource-id";
+    const executionId = "existing-execution-id";
 
-  test("Lock guards shared resource", async () => {
-    expect(sharedResource.length).toBe(0);
-
-    r1.run("write", "id", "a", false);
-    const p2 = r2.run("write", "id2", "b", true);
-
-    while (sharedResource.length === 0) {
-      await new Promise((resolve) => setTimeout(resolve, 10));
-    }
-    expect(sharedResource.length).toBe(2);
-
-    try {
-      // release lock so p2 can run
-      await store.locks.release("write/id", sharedResource[0]);
-    } catch (e) {
-      // continue even if lock is not released
-      console.error(e);
-    }
-    // try but not fail
-    const r = await p2;
-
-    expect(sharedResource.length).toBe(2);
-    expect(sharedResource).toContain("a");
-    expect(sharedResource).toContain("b");
-    expect(sharedResource).toEqual(r);
-  });
-
-  test("Lock store behaves correctly when acquiring and releasing locks", async () => {
-    const acquireResult = await store.locks.tryAcquire("resource-id-1", "execution-id-1");
+    // Acquire the lock
+    const acquireResult = await store.locks.tryAcquire(resourceId, executionId);
     expect(acquireResult).toBe(true);
 
-    await expect(store.locks.tryAcquire("resource-id-1", "execution-id-2")).rejects.toThrow();
+    // Attempt to acquire the lock again, should succeed
+    const secondAcquireResult = await store.locks.tryAcquire(resourceId, executionId);
+    expect(secondAcquireResult).toBe(true);
 
-    await store.locks.release("resource-id-1", "execution-id-1");
-    await expect(store.locks.release("resource-id-1", "execution-id-1")).rejects.toThrow();
+    // Release the lock to clean up
+    await store.locks.release(resourceId, executionId);
   });
 
-  test("Lock expiration works as expected", async () => {
-    if (!useDurable) {
-      return;
-    }
+  test("Lock Store: Acquire lock that exists with different executionId", async () => {
+    const resourceId = "existing-resource-id";
+    const executionId1 = "existing-execution-id-1";
+    const executionId2 = "existing-execution-id-2";
 
-    // Acquire a lock with a short expiration time
-    const remoteLockStore = new RemoteLockStore(url, "process-id", new Logger(), 1000);
-    const acquireResult = await remoteLockStore.tryAcquire("resource-id-3", "execution-id-1");
-    expect(acquireResult).toBe(true);
+    // Acquire the lock with executionId1
+    const acquireResult1 = await store.locks.tryAcquire(resourceId, executionId1);
+    expect(acquireResult1).toBe(true);
 
-    // Wait for the lock to expire
-    await new Promise((resolve) => setTimeout(resolve, 2000)); // Assuming lockExpiry is 60000
+    // Attempt to acquire the lock with executionId2, should fail
+    await expect(store.locks.tryAcquire(resourceId, executionId2)).rejects.toThrow();
 
-    // Attempt to release the expired lock, should fail
-    await expect(remoteLockStore.release("resource-id-3", "execution-id-1")).rejects.toThrow();
+    // Release the lock to clean up
+    await store.locks.release(resourceId, executionId1);
   });
 
-  test("Attempt to release a lock without acquiring it", async () => {
-    const nonAcquiredResourceId = "non-acquired-resource-id";
-    const nonAcquiredExecutionId = "non-acquired-execution-id";
+  test("Lock Store: Release lock that exists with same executionId", async () => {
+    const resourceId = "existing-resource-id";
+    const executionId = "existing-execution-id";
 
-    await expect(store.locks.release(nonAcquiredResourceId, nonAcquiredExecutionId)).rejects.toThrow();
+    // Acquire the lock
+    await store.locks.tryAcquire(resourceId, executionId);
+
+    // Release the lock, should succeed
+    await store.locks.release(resourceId, executionId);
+  });
+
+  test("Lock Store: Release lock that exists with different executionId", async () => {
+    const resourceId = "existing-resource-id";
+    const executionId1 = "existing-execution-id-1";
+    const executionId2 = "existing-execution-id-2";
+
+    // Acquire the lock with executionId1
+    await store.locks.tryAcquire(resourceId, executionId1);
+
+    // Attempt to release the lock with executionId2, should fail
+    await expect(store.locks.release(resourceId, executionId2)).rejects.toThrow();
+
+    // Release the lock to clean up
+    await store.locks.release(resourceId, executionId1);
+  });
+
+  test("Lock Store: Release lock that does not exist", async () => {
+    const nonExistingResourceId = "non-existing-resource-id";
+    const nonExistingExecutionId = "non-existing-execution-id";
+
+    // Attempt to release a lock that does not exist, should fail
+    await expect(store.locks.release(nonExistingResourceId, nonExistingExecutionId)).rejects.toThrow();
   });
 });
--- impl/sdk/ts/test/scheduler.test.ts
+++ impl/sdk/ts/test/scheduler.test.ts
@@ -7,9 +7,8 @@ import { Logger } from "../lib/core/loggers/logger";
 
 jest.setTimeout(10000);
 
-describe("Schedule Store", () => {
+describe("Store: Schedules", () => {
   let store: IStore;
-  const scheduleId = "schedule-1";
   const useDurable = process.env.USE_DURABLE === "true";
 
   beforeEach(() => {
@@ -17,48 +16,149 @@ describe("Schedule Store", () => {
     store = useDurable ? new RemoteStore(url, "", new Logger()) : new LocalStore();
   });
 
-  test("creates a schedule", async () => {
+  test("Schedule Store: Create schedule", async () => {
+    const scheduleId = "new-schedule";
     const cronExpression = "* * * * *"; // Every minute
-    const tags = { category: "testing" };
-    const promiseId = "promise-1";
-    const promiseTimeout = 60000; // 1 minute
-    const promiseHeaders = { "Content-Type": "application/json" };
-    const promiseData = '{"message": "Test"}';
-    const promiseTags = { priority: "high" };
 
-    await store.schedules.create(
+    const createdSchedule = await store.schedules.create(
       scheduleId,
       scheduleId,
-      "Test Schedule",
+      "New Schedule",
       cronExpression,
-      tags,
-      promiseId,
-      promiseTimeout,
-      promiseHeaders,
-      promiseData,
-      promiseTags,
+      {},
+      "promise-1",
+      60000,
+      {},
+      '{"message": "Test"}',
+      {},
     );
-
-    // get the schedule
-    const createdSchedule = await store.schedules.get(scheduleId);
 
     expect(createdSchedule.id).toBe(scheduleId);
     expect(createdSchedule.cron).toBe(cronExpression);
-    expect(createdSchedule.tags).toEqual(tags);
-    expect(createdSchedule.promiseId).toBe(promiseId);
-    expect(createdSchedule.promiseTimeout).toBe(promiseTimeout);
-    expect(createdSchedule.promiseParam?.headers).toEqual(promiseHeaders);
-    expect(createdSchedule.promiseTags).toEqual(promiseTags);
+
+    // Clean up
+    await store.schedules.delete(scheduleId);
   });
 
-  test("deletes a schedule", async () => {
-    const scheduleId = "schedule-to-delete";
+  // this test needs to be discussed
+  test("Schedule Store: Create schedule that exists with same idempotency key", async () => {
+    const scheduleId = "existing-schedule";
+    const cronExpression = "* * * * *"; // Every minute
 
-    // Create a schedule first
+    // Create the initial schedule
     await store.schedules.create(
       scheduleId,
       scheduleId,
-      "Test Schedule",
+      "Existing Schedule",
+      cronExpression,
+      {},
+      "promise-1",
+      60000,
+      {},
+      '{"message": "Test"}',
+      {},
+    );
+
+    const schedule = await store.schedules.create(
+      scheduleId,
+      scheduleId,
+      "Existing Schedule",
+      cronExpression,
+      {},
+      "promise-1",
+      60000,
+      {},
+      '{"message": "Test"}',
+      {},
+    );
+
+    expect(schedule.id).toBe(scheduleId);
+    expect(schedule.cron).toBe(cronExpression);
+    expect(schedule.idempotencyKey).toBe(scheduleId);
+
+    // Clean up
+    await store.schedules.delete(scheduleId);
+  });
+
+  test("Schedule Store: Create schedule that exists with different idempotency key", async () => {
+    const scheduleId = "existing-schedule";
+    const cronExpression = "* * * * *"; // Every minute
+
+    // Create the initial schedule
+    await store.schedules.create(
+      scheduleId,
+      scheduleId,
+      "Existing Schedule",
+      cronExpression,
+      {},
+      "promise-1",
+      60000,
+      {},
+      '{"message": "Test"}',
+      {},
+    );
+
+    // Attempt to create a schedule with a different idempotency key, should throw error
+    await expect(
+      store.schedules.create(
+        scheduleId,
+        "new-idempotency-key",
+        "New Schedule",
+        cronExpression,
+        {},
+        "promise-1",
+        60000,
+        {},
+        '{"message": "Test"}',
+        {},
+      ),
+    ).rejects.toThrowError("Already exists");
+
+    // Clean up
+    await store.schedules.delete(scheduleId);
+  });
+
+  test("Schedule Store: Get schedule that exists", async () => {
+    const scheduleId = "existing-schedule";
+    const cronExpression = "* * * * *"; // Every minute
+
+    // Create the schedule
+    await store.schedules.create(
+      scheduleId,
+      scheduleId,
+      "Existing Schedule",
+      cronExpression,
+      {},
+      "promise-1",
+      60000,
+      {},
+      '{"message": "Test"}',
+      {},
+    );
+
+    // Get the existing schedule
+    const existingSchedule = await store.schedules.get(scheduleId);
+    expect(existingSchedule.id).toBe(scheduleId);
+
+    // Clean up
+    await store.schedules.delete(scheduleId);
+  });
+
+  test("Schedule Store: Get schedule that does not exist", async () => {
+    const nonExistingScheduleId = "non-existing-schedule-id";
+
+    // Attempt to get a schedule that does not exist, should throw NOT_FOUND error
+    await expect(store.schedules.get(nonExistingScheduleId)).rejects.toThrowError("Not found");
+  });
+
+  test("Schedule Store: Delete schedule that exists", async () => {
+    const scheduleId = "schedule-to-delete";
+
+    // Create the schedule first
+    await store.schedules.create(
+      scheduleId,
+      scheduleId,
+      "Existing Schedule",
       "* * * * *",
       {},
       "promise-1",
@@ -68,66 +168,129 @@ describe("Schedule Store", () => {
       {},
     );
 
-    // search for the schedule
-    const searchResults = await store.schedules.get(scheduleId);
-    expect(searchResults.id).toBe(scheduleId);
-
+    // Attempt to delete the schedule
     const isDeleted = await store.schedules.delete(scheduleId);
     expect(isDeleted).toBeUndefined();
 
-    // search for the schedule again
+    // Attempt to get the deleted schedule, should throw NOT_FOUND error
+    await expect(store.schedules.get(scheduleId)).rejects.toThrowError("Not found");
+  });
+
+  test("Schedule Store: Delete schedule that does not exist", async () => {
+    const nonExistingScheduleId = "non-existing-schedule-id";
+
+    // Attempt to delete a schedule that does not exist, should throw NOT_FOUND error
+    await expect(store.schedules.delete(nonExistingScheduleId)).rejects.toThrowError("Not found");
+  });
+
+  test("Schedule Store: Search by id", async () => {
+    const scheduleId = "search-by-id-schedule";
+
+    // Create the schedule
+    await store.schedules.create(
+      scheduleId,
+      scheduleId,
+      "Search by ID Schedule",
+      "* * * * *",
+      {},
+      "promise-1",
+      60000,
+      {},
+      '{"message": "Test"}',
+      {},
+    );
+
+    // Search for the schedule by id
     let schedules: Schedule[] = [];
     for await (const searchResults of store.schedules.search(scheduleId, {})) {
       schedules = schedules.concat(searchResults);
     }
-    expect(schedules.length).toBe(0);
+    expect(schedules.length).toBe(1);
+    expect(schedules[0].id).toBe(scheduleId);
+
+    // Clean up
+    await store.schedules.delete(scheduleId);
   });
 
-  test("searches for schedules", async () => {
-    // Create multiple schedules for testing
-    await store.schedules.create(
-      "schedule-2",
-      "schedule-2",
-      "Test Schedule",
-      "* * * * *",
-      { category: "search testing" },
-      "promise-1",
-      60000,
-      {},
-      '{"message": "Test"}',
-      {},
-    );
+  test("Schedule Store: Search by id with wildcard(s)", async () => {
+    const scheduleIdPrefix = "should-match";
+    const wildcardSearch = `${scheduleIdPrefix}*`;
 
-    await store.schedules.create(
-      "schedule-3",
-      "schedule-3",
-      "Test Schedule",
-      "* * * * *",
-      { category: "search testing" },
-      "promise-2",
-      60000,
-      {},
-      '{"message": "Test"}',
-      {},
-    );
+    const scheduleIds = [
+      "should-match-1",
+      "should-match-2",
+      "should-match-3",
+      "should-not-match-1",
+      "should-not-match-2",
+      "should-not-match-3",
+    ];
 
-    // Expecting 1 schedules based on the search criteria
+    for (const i in scheduleIds) {
+      await store.schedules.create(
+        scheduleIds[i],
+        scheduleIds[i],
+        "Search by ID Prefix Schedule",
+        "* * * * *",
+        {},
+        `promise-${scheduleIds[i]}`,
+        60000,
+        {},
+        '{"message": "Test"}',
+        {},
+      );
+    }
+
+    // Search for schedules by id prefix with wildcard
     let schedules: Schedule[] = [];
-    for await (const searchResults of store.schedules.search("schedule-2", { category: "search testing" })) {
+    for await (const searchResults of store.schedules.search(wildcardSearch, {})) {
+      schedules = schedules.concat(searchResults);
+    }
+    expect(schedules.length).toBe(3);
+    // assert ids
+    expect(schedules.map((s) => s.id)).toEqual(expect.arrayContaining(scheduleIds.slice(0, 3)));
+
+    // Clean up
+    for (const i in scheduleIds) {
+      await store.schedules.delete(scheduleIds[i]);
+    }
+  });
+
+  test("Schedule Store: Search by tags", async () => {
+    const scheduleIds = ["search-by-tags-schedule", "should-not-match-1", "should-not-match-2"];
+
+    const scheduleId = "search-by-tags-schedule";
+    const searchtag = { category: "search testing", priority: "high" };
+
+    // Create the schedule with specific tags
+    for (const i in scheduleIds) {
+      let tags = { category: "search don't match", priority: "mid" };
+      if (scheduleIds[i] === scheduleId) {
+        tags = searchtag;
+      }
+      await store.schedules.create(
+        scheduleIds[i],
+        scheduleIds[i],
+        "Search by Tags Schedule",
+        "* * * * *",
+        tags,
+        `promise-${scheduleIds[i]}`,
+        60000,
+        {},
+        '{"message": "Test"}',
+        {},
+      );
+    }
+
+    // Search for the schedule by tags
+    let schedules: Schedule[] = [];
+    for await (const searchResults of store.schedules.search(scheduleId, searchtag)) {
       schedules = schedules.concat(searchResults);
     }
     expect(schedules.length).toBe(1);
-  });
 
-  test("search schedule with non-existing ID", async () => {
-    let schedules: Schedule[] = [];
-    for await (const searchResults of store.schedules.search("non-existing-id", {})) {
-      schedules = schedules.concat(searchResults);
+    // Clean up
+    for (const i in scheduleIds) {
+      await store.schedules.delete(scheduleIds[i]);
     }
-    expect(schedules.length).toBe(0);
-  });
-
-  test("delete non-existing schedule", async () => {
-    await expect(store.schedules.delete("non-existing-id")).rejects.toThrow();
   });
 });
