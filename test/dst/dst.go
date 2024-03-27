d12b433f9071138bcfc75e0941c068810e488669
Vipul Vaibhaw <vaibhaw.vipul@gmail.com>
2024-03-27 15:49:01 +0530

    add a check that req is not nil

--- test/dst/dst.go
+++ test/dst/dst.go
@@ -122,13 +122,18 @@ func (d *DST) Run(r *rand.Rand, api api.API, aio aio.AIO, system *system.System,
 			metadata.Tags.Set("name", req.Kind.String())
 			metadata.Tags.Set("api", "dst")
 
+			if req == nil {
+				errs = append(errs, fmt.Errorf("request is nil"))
+				break
+			}
+
 			api.Enqueue(&bus.SQE[t_api.Request, t_api.Response]{
 				Metadata:   metadata,
 				Submission: req,
 				Callback: func(res *t_api.Response, err error) {
 					// CQE Invariant: Assert that a callback is set after dequeuing
 					if res == nil && err == nil {
-						errs = append(errs, fmt.Errorf("CQE Invariant violation: callback is not set for response"))
+						errs = append(errs, fmt.Errorf("cqe invariant violation: callback is not set for response"))
 					} else {
 						modelErr := model.Step(t, req, res, err)
 						if modelErr != nil {
