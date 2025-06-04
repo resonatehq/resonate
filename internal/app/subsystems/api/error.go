8b2d36ad9b95c8b64d54300c77b029c78e45797f
David Farr <david@resonatehq.io>
2025-06-04 14:44:41 -0700

    Update error code link (#711)
    ---------
    
    Co-authored-by: Cully <cully.wakelin@resonatehq.io>

--- internal/app/subsystems/api/error.go
+++ internal/app/subsystems/api/error.go
@@ -56,7 +56,7 @@ func ServerError(err error) *Error {
 			Message: message,
 			Domain:  "server",
 			Metadata: map[string]string{
-				"url": fmt.Sprintf("https://docs.resonatehq.io/reference/error-codes#%d", error.Code()),
+				"url": fmt.Sprintf("https://docs.resonatehq.io/operate/errors#%d", error.Code()),
 			},
 		}},
 	}
@@ -71,7 +71,7 @@ func RequestError(status t_api.StatusCode) *Error {
 			Message: "Request errors are not retryable since they are caused by invalid client requests",
 			Domain:  "request",
 			Metadata: map[string]string{
-				"url": fmt.Sprintf("https://docs.resonatehq.io/reference/error-codes#%d", status),
+				"url": fmt.Sprintf("https://docs.resonatehq.io/operate/errors#%d", status),
 			},
 		}},
 	}
@@ -87,7 +87,7 @@ func RequestValidationError(err error) *Error {
 			Message: err,
 			Domain:  "request",
 			Metadata: map[string]string{
-				"url": fmt.Sprintf("https://docs.resonatehq.io/resonate/error-codes#%d", code),
+				"url": fmt.Sprintf("https://docs.resonatehq.io/operate/errors#%d", code),
 			},
 		})
 	}
