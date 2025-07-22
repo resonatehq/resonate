85c31d061ff5e3b2c2a2e5e30c1979e62e86b195
Cully Wakelin <cully.wakelin@resonatehq.io>
2025-07-22 16:56:45 -0600

    rm newline for linter

--- cmd/projects/list.go
+++ cmd/projects/list.go
@@ -44,7 +44,7 @@ func ListProjectCmd() *cobra.Command {
 }
 
 func display(templates Projects) {
-	fmt.Println("\n✨ Available templates ✨\n\n")
+	fmt.Println("\n✨ Available templates ✨\n")
 	for name, t := range templates {
 		fmt.Printf("✨ %s\n\n\t%s\n\n", name, t.Desc)
 		fmt.Printf("\tTo use this template, run:\n\n")
