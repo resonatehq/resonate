e09368d541b69f0ec86eb7f3f13fd3a385bea05b
Andres Villegas <andres.villegas@resonatehq.io>
2025-08-14 13:51:53 -0700

    skip dst tests (#203)

--- impl/sdk/ts/tests/dst.test.ts
+++ impl/sdk/ts/tests/dst.test.ts
@@ -1,7 +1,7 @@
 import { run } from "../sim/main";
 
 describe("run dst", () => {
-  test("should execute simulation and not ", () => {
+  test.skip("should execute simulation and not ", () => {
     expect(() =>
       run({
         seed: Math.floor(Math.random() * 1000000),
