0b1c684adf89e6d09811bd8c283a70d1535efed7
Andres Villegas <andres.villegas@resonatehq.io>
2025-08-18 15:25:01 -0700

    Allow string invocation from resonate class and use basic options (#211)

--- impl/sdk/ts/src/util.ts
+++ impl/sdk/ts/src/util.ts
@@ -54,7 +54,7 @@ export function isDurablePromiseRecord(obj: unknown): obj is DurablePromiseRecor
     (obj as any).tags !== null &&
     !Array.isArray((obj as any).tags) &&
     Object.values((obj as any).tags).every((v) => typeof v === "string") &&
-    ["pending", "resolved", "rejected", "rejected_canceled", "rejected_timedout"].includes((obj as any).state) &&
+    ["PENDING", "RESOLVED", "REJECTED", "REJECTED_CANCELED", "REJECTED_TIMEDOUT"].includes((obj as any).state) &&
     (typeof (obj as any).iKeyForCreate === "undefined" || typeof (obj as any).iKeyForCreate === "string") &&
     (typeof (obj as any).iKeyForComplete === "undefined" || typeof (obj as any).iKeyForComplete === "string") &&
     (typeof (obj as any).createdOn === "undefined" || typeof (obj as any).createdOn === "number") &&
