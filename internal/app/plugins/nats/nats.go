9dbd616e55b534098e1487d7fcd8e912d60f330a
muhammad-asghar-ali <masgharali.eng@gmail.com>
2025-11-02 01:30:24 +0500

    chore(): add head in nats plugin process func

--- internal/app/plugins/nats/nats.go
+++ internal/app/plugins/nats/nats.go
@@ -40,7 +40,7 @@ type processor struct {
 	timeout time.Duration
 }
 
-func (p *processor) Process(body []byte, data []byte) (bool, error) {
+func (p *processor) Process(body []byte, head map[string]string, data []byte) (bool, error) {
 	var addr *Addr
 	if err := json.Unmarshal(data, &addr); err != nil {
 		return false, err
