656257c4f644764aa12a65bf340e68bd289454b2
David Farr <david@resonatehq.io>
2025-11-11 14:20:06 -0800

    Bump version

--- internal/version.go
+++ internal/version.go
@@ -1,5 +1,5 @@
 package internal
 
 func Version() string {
-	return "0.7.22" // This needs to be bumped when new versions are released.
+	return "0.7.23" // This needs to be bumped when new versions are released.
 }
