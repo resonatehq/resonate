91ce66ed6105dfa1108b1380904dc81fb5193614
David Farr <david@resonatehq.io>
2025-12-04 19:35:11 -0800

    Add loop in background coroutines to clear backlog

--- internal/app/coroutines/schedulePromises.go
+++ internal/app/coroutines/schedulePromises.go
@@ -18,94 +18,99 @@ func SchedulePromises(c gocoro.Coroutine[*t_aio.Submission, *t_aio.Completion, a
 
 	config := c.Get("config").(*system.Config)
 
-	// read elapsed schedules
-	completion, err := gocoro.YieldAndAwait(c, &t_aio.Submission{
-		Kind: t_aio.Store,
-		Tags: m,
-		Store: &t_aio.StoreSubmission{
-			Transaction: &t_aio.Transaction{
-				Commands: []t_aio.Command{
-					&t_aio.ReadSchedulesCommand{
-						NextRunTime: c.Time(),
-						Limit:       config.ScheduleBatchSize,
+	for i := 0; i < config.ScheduleMaxIterations; i++ {
+		// read elapsed schedules
+		completion, err := gocoro.YieldAndAwait(c, &t_aio.Submission{
+			Kind: t_aio.Store,
+			Tags: m,
+			Store: &t_aio.StoreSubmission{
+				Transaction: &t_aio.Transaction{
+					Commands: []t_aio.Command{
+						&t_aio.ReadSchedulesCommand{
+							NextRunTime: c.Time(),
+							Limit:       config.ScheduleBatchSize,
+						},
 					},
 				},
 			},
-		},
-	})
-	if err != nil {
-		slog.Error("failed to read schedules", "err", err)
-		return nil, nil
-	}
-
-	util.Assert(completion.Store != nil, "completion must not be nil")
-	util.Assert(len(completion.Store.Results) == 1, "completion must have one result")
-
-	result := t_aio.AsQuerySchedules(completion.Store.Results[0])
-	util.Assert(result != nil, "result must not be nil")
-
-	awaiting := make([]gocoroPromise.Awaitable[*promiseAndTask], len(result.Records))
-	commands := make([]*t_aio.CreatePromiseCommand, len(result.Records))
-
-	for i, r := range result.Records {
-		util.Assert(r.NextRunTime <= c.Time(), "schedule next run time must have elapsed")
-
-		s, err := r.Schedule()
-		if err != nil {
-			slog.Error("failed to parse schedule, skipping", "err", err)
-			continue
-		}
-
-		next, err := util.Next(s.NextRunTime, s.Cron)
-		if err != nil {
-			slog.Error("failed to calculate next run time for schedule, skipping", "err", err)
-			continue
-		}
-		id, err := generatePromiseId(s.PromiseId, map[string]string{
-			"id":        s.Id,
-			"timestamp": fmt.Sprintf("%d", s.NextRunTime),
 		})
-
 		if err != nil {
-			slog.Error("failed to generate promise id for scheduled promise, skipping", "err", err)
-			continue
+			slog.Error("failed to read schedules", "err", err)
+			return nil, nil
 		}
 
-		// add invocation tags
-		s.PromiseTags["resonate:schedule"] = s.Id
-		s.PromiseTags["resonate:invocation"] = "true"
+		util.Assert(completion.Store != nil, "completion must not be nil")
+		util.Assert(len(completion.Store.Results) == 1, "completion must have one result")
 
-		// create promise command
-		commands[i] = &t_aio.CreatePromiseCommand{
-			Id:        id,
-			Param:     s.PromiseParam,
-			Timeout:   s.PromiseTimeout + s.NextRunTime,
-			Tags:      s.PromiseTags,
-			CreatedOn: c.Time(),
+		result := t_aio.AsQuerySchedules(completion.Store.Results[0])
+		if len(result.Records) == 0 {
+			break
 		}
 
-		awaiting[i] = gocoro.Spawn(c, createPromise(m, nil, commands[i], nil, &t_aio.UpdateScheduleCommand{
-			Id:          s.Id,
-			LastRunTime: &s.NextRunTime,
-			NextRunTime: next,
-		}))
+		awaiting := make([]gocoroPromise.Awaitable[*promiseAndTask], len(result.Records))
+		commands := make([]*t_aio.CreatePromiseCommand, len(result.Records))
+
+		for i, r := range result.Records {
+			util.Assert(r.NextRunTime <= c.Time(), "schedule next run time must have elapsed")
+
+			s, err := r.Schedule()
+			if err != nil {
+				slog.Error("failed to parse schedule, skipping", "err", err)
+				continue
+			}
+
+			next, err := util.Next(s.NextRunTime, s.Cron)
+			if err != nil {
+				slog.Error("failed to calculate next run time for schedule, skipping", "err", err)
+				continue
+			}
+			id, err := generatePromiseId(s.PromiseId, map[string]string{
+				"id":        s.Id,
+				"timestamp": fmt.Sprintf("%d", s.NextRunTime),
+			})
+
+			if err != nil {
+				slog.Error("failed to generate promise id for scheduled promise, skipping", "err", err)
+				continue
+			}
+
+			// add invocation tags
+			s.PromiseTags["resonate:schedule"] = s.Id
+			s.PromiseTags["resonate:invocation"] = "true"
+
+			// create promise command
+			commands[i] = &t_aio.CreatePromiseCommand{
+				Id:        id,
+				Param:     s.PromiseParam,
+				Timeout:   s.PromiseTimeout + s.NextRunTime,
+				Tags:      s.PromiseTags,
+				CreatedOn: c.Time(),
+			}
+
+			awaiting[i] = gocoro.Spawn(c, createPromise(m, nil, commands[i], nil, &t_aio.UpdateScheduleCommand{
+				Id:          s.Id,
+				LastRunTime: &s.NextRunTime,
+				NextRunTime: next,
+			}))
+		}
+
+		for i := range awaiting {
+			if awaiting[i] == nil {
+				continue
+			}
+
+			completion, err := gocoro.Await(c, awaiting[i])
+			if err != nil {
+				slog.Error("failed to schedule promise", "err", err)
+				continue
+			}
+
+			if !completion.created {
+				slog.Warn("promise to be scheduled already exists", "promise", commands[i].Id, "schedule", result.Records[i].Id)
+			}
+		}
 	}
 
-	for i := range awaiting {
-		if awaiting[i] == nil {
-			continue
-		}
-
-		completion, err := gocoro.Await(c, awaiting[i])
-		if err != nil {
-			slog.Error("failed to schedule promise", "err", err)
-			continue
-		}
-
-		if !completion.created {
-			slog.Warn("promise to be scheduled already exists", "promise", commands[i].Id, "schedule", result.Records[i].Id)
-		}
-	}
 	return nil, nil
 }
 
