8c7ed3cd67c6aa818d980f14d958f3de4aeaaa00
Tomás Pérez Álvarez <72174660+Tomperez98@users.noreply.github.com>
2025-12-30 14:13:49 -0500

    Tomas/move deps for shims (#394)
    * move deps for shells to the index
    
    * update version

--- impl/sdk/ts/src/index.ts
+++ impl/sdk/ts/src/index.ts
@@ -2,10 +2,16 @@ export { WallClock } from "./clock";
 export { Context } from "./context";
 export * as core from "./core";
 export { JsonEncoder } from "./encoder";
+export {
+  type Encryptor,
+  NoopEncryptor,
+} from "./encryptor";
 export { Handler } from "./handler";
 export { AsyncHeartbeat, NoopHeartbeat } from "./heartbeat";
 export { HttpNetwork } from "./network/remote";
+export { OptionsBuilder } from "./options";
 export { Registry } from "./registry";
 export { Resonate, ResonateFunc, ResonateHandle } from "./resonate";
 export { ResonateInner, type Task } from "./resonate-inner";
+export { NoopTracer } from "./tracer";
 export { Func } from "./types";
